bbd4d866b3c7bedca1dbbbba11557238df21443b
Ryan Cope <rcope@1800radiator.com>
2023-09-23 23:33:27 -0700

    Adding documentation to funcs

--- modules/slack/slack.py
+++ modules/slack/slack.py
@@ -3,7 +3,25 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 
-def _build_message(message: str, users: list = None, code: str = None):
+def _build_message(message: str, users: list = None, code: str = None) -> str:
+    """Builds a message string for the `message_slack_channel` function
+
+    Parameters
+    ----------
+        message : str
+            Body of message
+        users (list, optional): list, default None
+            List of users to mention in the message
+                Note: must have `config.py` setup with the `USERS` dictionary like so:
+                    `{User: ID}`
+        code (str, optional): str, default None
+            Code block to add to message
+
+    Returns
+    -------
+        str
+            Message string
+    """
     if users:
         message = (
             (", ".join([f"<@{USERS.get(usr)}>" for usr in users]))
@@ -23,6 +41,23 @@ def message_slack_channel(
     file: str = None,
     mention_users: list = None,
 ):
+    """Send a message to a slack channel
+
+    Parameters
+    ----------
+        message : str
+            Body of message
+        channel (str, optional): str, default CHANNEL
+            Channel name ex `#channelname`
+        code (str, optional): str, default None
+            Code block for message string
+        file (str, optional): str, default None
+            Local path to file
+        mention_users (list, optional): list, default None
+            List of users to mention in the message
+                Note: must have `config.py` setup with the `USERS` dictionary like so:
+                    `{User: ID}`
+    """
     slack_client = WebClient(token=TOKEN)
 
     message = _build_message(message=message, users=mention_users, code=code)
--- modules/twilio/twilio.py
+++ modules/twilio/twilio.py
@@ -3,5 +3,14 @@ from twilio.rest import Client
 
 
 def send_twilio_message(message: str, to_phone: str = TO_PHONE):
+    """Sends a text message via Twilio API
+
+    Parameters
+    ----------
+        message : str
+            Message you'd like to send
+        to_phone (str, optional): str, default TO_PHONE
+            Recipient phone number (10 digit)
+    """
     twilio_client = Client(SID, TOKEN)
     message = twilio_client.messages.create(to=to_phone, from_=FROM_PHONE, body=message)
